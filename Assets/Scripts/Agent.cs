44baf21e143be07cdb6b7cae9f58457875488558
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-03-22 13:52:05 +0000

    basic movement

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Agent : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public void MoveTo(Vector3 destination)
+    {
+        navMeshAgent.destination = destination;
+        navMeshAgent.isStopped = false;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Agent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<Agent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            InteractWithMovement();
+        }
+    }
+
+    private void InteractWithMovement()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction, out hit))
+        {
+            agent.MoveTo(hit.point);
+        }
+    }
+}
