8a2915ecbed521de21929c0d64dac7044f4ffb83
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-03-22 14:56:46 +0000

    simple action

---
+++ Assets/Scripts/Action.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Action", menuName = "GOAP/Create New Action", order = 0)]
+public class Action : ScriptableObject
+{
+    [SerializeField] string locationTag = null;
+    [SerializeField] float duration = 0f;
+
+    public string GetName() { return name; }
+    public float GetDuration() { return duration; }
+
+    public Transform GetLocation() {
+        if (locationTag == null) { return null; }
+
+        GameObject[] locations = GameObject.FindGameObjectsWithTag(locationTag);
+        if (locations.Length <= 0) { return null; }
+
+        GameObject location = locations[Random.Range(0, locations.Length - 1)];
+        return location.transform;
+    }
+}
--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -1,19 +1,54 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class Agent : MonoBehaviour
 {
+    [SerializeField] float withinTargetRange = 2f;
+    [SerializeField] Action[] actions = null;
+
     NavMeshAgent navMeshAgent;
+    Action currentAction;
+    Vector3 currentDestination;
 
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    public void MoveTo(Vector3 destination)
+    private void Start()
     {
-        navMeshAgent.destination = destination;
+        if (actions.Length > 0)
+        {
+            currentAction = actions[0];
+            MoveTo(actions[0].GetLocation().position);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (currentDestination == Vector3.zero) { return; }
+
+        float distanceToTarget = Vector3.Distance(currentDestination, this.transform.position);
+        if (distanceToTarget < withinTargetRange)
+        {
+            StartCoroutine(DoAction());
+        }
+    }
+
+    public void MoveTo(Vector3 newDestination)
+    {
+        currentDestination = newDestination;
+        navMeshAgent.destination = newDestination;
         navMeshAgent.isStopped = false;
+    }
+
+    private IEnumerator DoAction()
+    {
+        navMeshAgent.isStopped = true;
+        yield return new WaitForSeconds(currentAction.GetDuration());
+        currentAction = null;
+        currentDestination = Vector3.zero;
     }
 }
