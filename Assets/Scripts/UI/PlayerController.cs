7c2e813801c05c29eb9d22a92de2466e7d67fa35
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-03-24 11:16:28 +0000

    Update player controller

---
+++ Assets/Scripts/UI/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Ecosystem.UI
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class PlayerController : MonoBehaviour
+    {
+        NavMeshAgent navMeshAgent;
+
+        private void Awake()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                InteractWithMovement();
+            }
+        }
+
+        private void InteractWithMovement()
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out hit))
+            {
+                navMeshAgent.destination = hit.point;
+            }
+        }
+    }
+}
