c8b142c31118f16eba796a372460be89a1bec36e
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-03-27 17:21:46 +0000

    Population monitor UI

---
+++ Assets/Scripts/UI/PopulationMonitor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ecosystem.UI
+{
+    public class PopulationMonitor : MonoBehaviour
+    {
+        [SerializeField] Transform populationParent = null;
+        [SerializeField] Text populationText = null;
+        [SerializeField] Text birthsText = null;
+        [SerializeField] Text deathsText = null;
+
+        List<GameObject> lastPopulation = new List<GameObject>();
+        int populationCount = 0;
+        int birthsCount = 0;
+        int deathsCount = 0;
+
+        private void LateUpdate()
+        {
+            List<GameObject> currentPopulation = new List<GameObject>();
+            foreach (Transform child in populationParent)
+            {
+                currentPopulation.Add(child.gameObject);
+                if (!lastPopulation.Contains(child.gameObject))
+                {
+                    birthsCount++;
+                }
+            }
+
+            foreach (GameObject individual in lastPopulation)
+            {
+                if (!currentPopulation.Contains(individual))
+                {
+                    deathsCount++;
+                }
+            }
+            lastPopulation = currentPopulation;
+            populationCount = currentPopulation.Count;
+
+            populationText.text = populationCount.ToString();
+            birthsText.text = birthsCount.ToString();
+            deathsText.text = deathsCount.ToString();
+        }
+    }
+}
