d35123041a2fc0f3471954dbd1195abd3b2a6bf0
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-03-31 16:16:02 +0100

    Convert to regular mesh script

---
+++ Assets/Scripts/Utils/ConvertToRegularMesh.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Dev.Utils
+{
+    public class ConvertToRegularMesh : MonoBehaviour
+    {
+        [ContextMenu("Convert to regular mesh")]
+        private void Convert()
+        {
+            SkinnedMeshRenderer skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+            MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+            MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+
+            meshFilter.sharedMesh = skinnedMeshRenderer.sharedMesh;
+            meshRenderer.sharedMaterials = skinnedMeshRenderer.sharedMaterials;
+
+            DestroyImmediate(skinnedMeshRenderer);
+            DestroyImmediate(this);
+        }
+    }
+}
