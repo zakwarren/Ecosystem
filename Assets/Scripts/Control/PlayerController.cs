7d09f26744f21a8781076f257fcbd6069c7b17f1
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-03-31 20:41:14 +0100

    Player controller

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -3,42 +3,49 @@ using UnityEngine.AI;
 
 namespace Ecosystem.Control
 {
-    [RequireComponent(typeof(NavMeshAgent))]
+    [RequireComponent(typeof(CharacterController))]
     public class PlayerController : MonoBehaviour
     {
         [SerializeField] float animationSmoothTime = 0.1f;
+        [SerializeField] float movementSpeed = 250f;
+        [SerializeField] float turnSpeed = 60f;
 
-        NavMeshAgent navMeshAgent;
+        // NavMeshAgent navMeshAgent;
         Animator animator;
+        CharacterController controller;
 
         private void Awake()
         {
-            navMeshAgent = GetComponent<NavMeshAgent>();
             animator = GetComponentInChildren<Animator>();
+            controller = GetComponent<CharacterController>();
         }
 
         private void Update()
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                InteractWithMovement();
-            }
+            
+            ProcessTranslation();
+            ProcessRotation();
+
             SetMovementAnimation();
         }
 
-        private void InteractWithMovement()
+        private void ProcessTranslation()
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray.origin, ray.direction, out hit))
-            {
-                navMeshAgent.destination = hit.point;
-            }
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            float speed = Input.GetAxis("Vertical") * movementSpeed;
+            controller.SimpleMove(forward * speed);
+        }
+
+        private void ProcessRotation()
+        {
+            float yThrow = Input.GetAxis("Horizontal");
+            float yaw = yThrow * turnSpeed * Time.deltaTime;
+            transform.Rotate(0, yaw, 0);
         }
 
         private void SetMovementAnimation()
         {
-            float speedPercent = navMeshAgent.velocity.magnitude / navMeshAgent.speed;
+            float speedPercent = controller.velocity.magnitude / movementSpeed;
             animator.SetFloat("speedPercent", speedPercent, animationSmoothTime, Time.deltaTime);
         }
     }
